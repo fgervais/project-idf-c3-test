9ffe9ec061b5252794845663a1b6ccee8fe13d44
Francois Gervais <francoisgervais@gmail.com>
2022-01-04 10:45:45 -0500

    Create a test main.cpp

---
+++ first_arduino_test/main/main.cpp
@@ -0,0 +1,36 @@
+void init_wifi() {
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	Serial.print("Connecting to WiFi ..");
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print('.');
+		delay(1000);
+	}
+	Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+	// initialize LED digital pin as an output.
+	pinMode(LED, OUTPUT);
+	// Serial.begin(115200);
+
+	// init_wifi();
+}
+
+void loop()
+{
+	// turn the LED on (HIGH is the voltage level)
+	digitalWrite(LED, HIGH);
+
+	// wait for a second
+	delay(1000);
+
+	// turn the LED off by making the voltage LOW
+	digitalWrite(LED, LOW);
+
+	// wait for a second
+	delay(1000);
+
+	Serial.print("Loop done");
+}
